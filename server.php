a2f590481c53a02410daadb79b3e2115c4f11e2d
luca <luca.carnaroli98@gmail.com>
2020-02-25 17:44:51 +0100

    es

--- server.php
+++ server.php
@@ -9,34 +9,34 @@ $database = [
       'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'The wall',
+      'author' => 'Pink Floyd',
+      'year' => 1979,
+      'poster' => 'https://img.discogs.com/DYVVPNBI0LT1iA0Laa7ttGAdYng=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-2041618-1390679025-2372.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
+      'title' => 'Master Of Puppets',
+      'author' => 'Metallica',
       'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'poster' => 'https://img.discogs.com/nypNrYyMw4BZc11Cij4z39BgPM0=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-1549636-1265948094.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
+      'title' => 'Lust For Life',
+      'author' => 'Iggy pop',
       'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'poster' => 'https://img.discogs.com/zO22jJSSGwM_dkfHnWhByRJ7Hk8=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-933439-1358398344-2097.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'Gone',
+      'author' => 'U2',
+      'year' => 1997,
+      'poster' => 'https://img.discogs.com/zsExS0Pc5T0YXmbQLdTHt9UizuY=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-137130-1460131020-9490.jpeg.jpg'
     ],
     [
-      'title' => 'New Jersey',
-      'author' => 'Bon Jovi',
-      'year' => 1988,
-      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+      'title' => 'Year Of The Gentleman',
+      'author' => 'Ne-Yo',
+      'year' => 2008,
+      'poster' => 'https://img.discogs.com/ko3OQ3ENyKrsdbpiU3kEPRGEH74=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-14826505-1582359304-8022.jpeg.jpg'
     ],
     [
       'title' => 'New Jersey',
